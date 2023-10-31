3f1b094fb8161dadf74fe53b56ecddaacd6f6f86
mahamealfred <mahamealfred@gmail.com>
2023-10-31 14:26:26 +0200

    cd2

--- db/db.js
+++ db/db.js
@@ -7,14 +7,33 @@ const username=process.env.DB_USER;
 const password=process.env.DB_PASS;
 const db_name=process.env.DB_NAME
 
+
 const dbConnect = async (next) => {
-    const conn= await mysql.createConnection({
+  console.log("database:",host,username,password,db_name)
+    const pool= await mysql.createPool({
+      connectionLimit: 10,
+      acquireTimeout: 60000, //30 secs
       host: host,
       user: username,
       password: password,
-      database: db_name
+      database: db_name,
+      // port: 3306,
+      // dialect: 'mysql',
       
     });
-    console.log(` connected on MySql.`);
+    pool.getConnection(function (err, connection) {
+      if (err) {
+          console.log(err);
+      } else {
+    console.log("connected..")
+          // var query1 = 'select * from table'
+          // connection.query(query1, function (error, results, fields) {
+          //     res.render('thispage', { user: req.user, results: results });
+
+          // });
+          connection.release();
+      }
+      });
+   
  };
-  export default dbConnect;
+  export default dbConnect; 
